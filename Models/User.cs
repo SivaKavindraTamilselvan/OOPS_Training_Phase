1d5f452ff972953327cecf21b3735565cc5beb83
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-04 14:20:14 +0530

    Creation of Email Notification App

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+namespace NotificationApp.Models;
+
+internal class User
+{
+    public string Name {get;set;}
+    public string Email {get;set;}
+    public string PhoneNumber{get;set;}
+
+    public User(string name,string email,string phonenumber)
+    {
+        Name = name;
+        Email = email;
+        PhoneNumber = phonenumber;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using NotificationApp.Validation;
+using NotificationApp.Models;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        User user = new User("Siva Kavindra","sivakavindra@gmail.com","9442378188");
+        bool result = EmailValidation.isValidEmail("sivakavindra");
+        Console.WriteLine(result);
+    }
+}

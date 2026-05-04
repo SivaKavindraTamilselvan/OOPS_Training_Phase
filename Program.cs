71fca4d9a99de820e870c26aa8735cd48083b474
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-04 17:03:00 +0530

    removed the comment lines

--- Program.cs
+++ Program.cs
@@ -10,16 +10,6 @@ internal class Program
     static void Main(string[] args)
     {
         Env.Load();
-
-        //User user = new User("Siva Kavindra","sivakavindratamilselvan@gmail.com","9442378188");
-        //bool result = EmailValidation.isValidEmail("sivakavindra");
-        //Console.WriteLine(result);
-        //result = PhoneNumberValidation.isValidPhoneNumber("9442378188");
-        //Console.WriteLine(result);
-        //EmailService email= new EmailService();
-        //email.Send("hii",user);
-        //SMSService sms = new SMSService();
-        //sms.Send("hii",user);
         UserService userService = new UserService();
         while(true)
         {

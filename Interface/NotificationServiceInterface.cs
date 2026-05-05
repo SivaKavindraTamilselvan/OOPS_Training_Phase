8e364a975892cef8f3f27b2fe04202a288a6f41a
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-05 20:00:33 +0530

    Adding the comment line

--- Interface/NotificationServiceInterface.cs
+++ Interface/NotificationServiceInterface.cs
@@ -3,11 +3,16 @@ namespace NotificationApp.Interfaces;
 
 internal interface INotification
 {
+    //interface defined for both email and sms notification
     void Send(string message,User user);
+
+    //check condition for valid email or phone
     private bool CheckValidation(User user)
     {
         return user != null;
     }
+
+    //enter the log in console alone
     private void Log(string message,User user)
     {
         Console.WriteLine($"Log: {message} for {user.Name}");
